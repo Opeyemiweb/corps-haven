54c077e3ca167e19addaf1244b2c7ba55cfb5c4a
Opeyemiweb <danielopeyemi1954@gmail.com>
2026-09-03 13:23:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 // its Supabase connection details from here.
 // ============================================================
 
-const SUPABASE_URL = "https://dqkiucqxpkionzaqiujc.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://dqkiucqxpkionzaqiujc.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxa2l1Y3F4cGtpb256YXFpdWpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyOTc3NTIsImV4cCI6MjEwMzg3Mzc1Mn0.AkCrfJq8fsc4NxwLDkFF2-FjQCNCo71Bf14Z5soLOVY";
